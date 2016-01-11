077d78edba1aa34bf55f0255b710e6e9ceaeee3c
Omer Yariv <oyariv@t19s.com>
2016-01-11 15:54:11 -0500

    basic content structure

--- index.ios.js
+++ index.ios.js
@@ -12,7 +12,62 @@ var {
   View,
 } = React;
 
+const PAGES = [
+  //first page
+  {
+    nodes : [
+      {
+        type : 'text',
+        content : {
+          'en' : 'test',
+          'es' : 'prueba'
+        }
+      },
+      {
+        type : 'text',
+        content : {
+          'en' : 'number one',
+          'es' : 'numero uno'
+        }
+      },
+      {
+        type : 'text',
+        content : {
+          'en' : 'is done',
+          'es' : 'completo'
+        }
+      }
+    ]
+  },
 
+  //2nd page
+  {
+    nodes : [
+      {
+        type : 'text',
+        content : {
+          'en' : 'Well, that\'s certainly good to know.',
+          'es' : 'Bueno, eso es ciertamente bueno saberlo.'
+        }
+      },
+      {
+        type : 'text',
+        content : {
+          'en' : 'I suggest you drop it, Mr. Data.',
+          'es' : 'Sugiero se te cae, Sr. Data.'
+        }
+      },
+      {
+        type : 'text',
+        content : {
+          'en' : 'Captain, why are we out here chasing comets?',
+          'es' : 'Capitán, ¿por qué estamos aquí persiguiendo cometas?'
+        }
+      }
+    ]
+  }
+
+];
 
 var blossom = React.createClass({
   render: function() {
