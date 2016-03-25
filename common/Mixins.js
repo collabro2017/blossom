83f678d695e7402e2da337c9f5ff70d6ef3caf11
Omer Yariv <oyariv@t19s.com>
2016-03-25 12:48:48 -0400

    buffed up speech a bit

--- common/Mixins.js
+++ common/Mixins.js
@@ -11,9 +11,18 @@ var Mixins = {
 
   speak : function(text, voice) {
     Speech.speak({
-          text: text,
-          voice: voice
-        })
+      text: text,
+      voice: voice,
+      // rate: 0.5, => //TODO: seems like iOS8 plays much faster??
+    }).then(started => {
+      console.log('started');
+      //nothing to do
+    })
+    .catch(error => {
+      console.log('error');
+      //nothing to do, but this may be raised when flipping pages
+      //while speech is playing
+    });
   },
 };
 
